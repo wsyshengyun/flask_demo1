74178aaf5320479a14c9444b1ee0a75b9d1b313a
wsy <wsy8718@163.com>
2021-11-24 22:36:02 +0800

    完成表单的模板的编写

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app 
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello,  World"
+    user = {'username' : 'Migue'}
+    posts = [ 
+            {
+                'author':{'username':'John'}, 
+                'body':'Beautiful day in Portland!'
+            },
+            {
+                'author':{'username':'Susan'}, 
+                'body':'The Avengers movie was so cool!'
+            }
+    ]
+    title = "Home"
+    return render_template('index.html', user=user
+    , title=title
+    , posts = posts
+    )
 
