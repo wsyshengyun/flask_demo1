eee4606692ffdd7fa3f829b2476be818dae5d599
wsy <wsy8718@163.com>
2021-11-27 22:29:43 +0800

    用户登出功能

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 # from models import *
+from flask_login import LoginManager
+
 
 
 
@@ -12,7 +14,8 @@ app.config.from_object(Config) # 此app就是Flask；
 app.debug=True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
+login.login_view = 'login'
 from  app import routes
 
 
--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
+from app import login
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login.user_loader
+def log_user(id):
+    return User.query.gete(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
