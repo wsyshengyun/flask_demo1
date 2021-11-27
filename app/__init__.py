256dc2f9e8ef8a6e3024fa5ec4f75041b45ef632
wsy <wsy8718@163.com>
2021-11-27 14:55:41 +0800

    数据库功能学习开始

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from os import truncate
 from  flask import Flask 
 from config import Config
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+
 
 
 app = Flask(__name__)
 app.config.from_object(Config) # 此app就是Flask；
 app.debug=True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from  app import routes
 
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     DEBUG = True
+    # 配置数据库相关的内容
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # 数据库发生变更后是否发送信号给应用
