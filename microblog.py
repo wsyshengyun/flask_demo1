7050e851489cf72783d92c4a6a86b2e97501ea72
wsy <wsy8718@163.com>
2021-11-27 16:49:18 +0800

    开始用户登陆的功能

--- microblog.py
+++ microblog.py
@@ -1,2 +1,9 @@
 from app import app
+from app import db 
 from app.models import *   # 解决数据库无法适移的问题
+
+
+# 添加一个shell上下文
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
