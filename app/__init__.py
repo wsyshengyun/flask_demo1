005fa733c5556fbb1b43de03f019e3ebabaf84d7
wsy <wsy8718@163.com>
2021-11-27 16:15:22 +0800

    解决数据库无法迁移的问题

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from  flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
+# from models import *
 
 
 
--- microblog.py
+++ microblog.py
@@ -1,1 +1,2 @@
 from app import app
+from app.models import *   # 解决数据库无法适移的问题
