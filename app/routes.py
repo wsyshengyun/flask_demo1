960d74abfe2fcb3b3803e65780a30a021418e008
wsy <wsy8718@163.com>
2021-11-28 21:58:14 +0800

    一些修改

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,9 @@
-from flask import render_template ,flash, redirect, url_for
-from app import app 
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask import render_template ,flash, redirect, url_for,request
+from app import app , db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
-from flask_login import logout_user
-from flask_login import login_required
-from flask import request
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app import db
-from app.forms import RegistrationForm
 
 
 
@@ -22,7 +17,7 @@ def logout():
 @app.route('/index')
 @login_required
 def index():
-    user = {'username' : 'Migue'}
+    # user = {'username' : 'Migue'}
     posts = [ 
             {
                 'author':{'username':'John'}, 
@@ -34,7 +29,7 @@ def index():
             }
     ]
     title = "Home"
-    return render_template('index.html', user=user
+    return render_template('index.html'
     , title=title
     , posts = posts
     )
