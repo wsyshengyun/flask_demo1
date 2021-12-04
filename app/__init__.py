58fd3891d5b76c49e06643ffadcc678d41e0d894
wsy <wsy8718@163.com>
2021-12-04 14:49:08 +0800

    添加日志功能和邮件功能

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,16 @@ from flask_login import LoginManager
 
 from flask_avatars import Avatars
 
+import logging
+from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
+import os 
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config) # 此app就是Flask；
-app.debug=True
+app.debug=False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -21,6 +27,38 @@ login.login_view = 'login'
 
 avatars = Avatars(app)
 
+# -----------------------------------------------------------
+# 添加邮件功能
+# -----------------------------------------------------------
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['mail_username'] or app.config['mail_password']:
+            auth = (app.config['mail_username'], app.config['mail_password'])
+        secure=None
+        if app.config['mail_use_tls']:
+            secure=()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['mail_server'], app.config['mail_port'])
+            ,fromaddr='no-reply@' + app.config['mail_server']
+            ,toaddrs=app.config['admins'] 
+            ,subject='Microblog Failure'
+            ,credentials=auth
+            ,secure=secure
+        )
+# 记录日志到文件
+if not app.debug:
+    if not os.path.exists('logs'): # TODO 理解
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+    
 from  app import routes,models, errors
 
 
--- config.py
+++ config.py
@@ -9,3 +9,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///'+os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # 数据库发生变更后是否发送信号给应用
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    admins = ['wsy8718@163.com']
